64450034a0ae9aa5e6e610ceefd91fe00311de22
Diwakar Kumar <51357882+Diwakarkumar3096@users.noreply.github.com>
2023-11-02 23:51:33 +0530

    Add files via upload
    01_varibale

---
+++ 01_varible.js
@@ -0,0 +1,8 @@
+console.log("Diwakar");
+const accountId="123456"
+var accountemail="diwakarkumar@gmail.com";
+let accountCity="Jaipur"
+
+accountemail="diwakar@gmail";
+accountCity="Banka";
+console.table([accountId,accountCity,accountemail]);
