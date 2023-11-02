3e830a49b28d864163aa32cadfb785a76949d4a0
Diwakar Kumar <51357882+Diwakarkumar3096@users.noreply.github.com>
2023-11-02 23:52:31 +0530

    Add files via upload
    02_datatype

---
+++ 02_datatype.js
@@ -0,0 +1,20 @@
+"use strict";//treat all Js code as newer version
+
+//alert(3+3);
+console.log(3+3);
+console.log("Diwakar");
+
+Number=12;
+BigInt=1234567890998765432;
+
+String="String";
+Boolean=true;
+
+undefined;
+null;
+Symbol=unique;
+Object={[
+    "aman",
+    "raman"
+]
+}
